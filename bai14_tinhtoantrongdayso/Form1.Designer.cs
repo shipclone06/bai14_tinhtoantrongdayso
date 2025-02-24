f72e507c0eb1e1e71bfcd273021bd4b6640b8219
shipclone06 <anhkhoa030900@gmail.com>
2025-02-24 15:45:51 +0700

    Add project files.

---
+++ bai14_tinhtoantrongdayso/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace bai14_tinhtoantrongdayso
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtsodau = new System.Windows.Forms.TextBox();
+            this.txtsocuoi = new System.Windows.Forms.TextBox();
+            this.txttongchan = new System.Windows.Forms.TextBox();
+            this.txttongle = new System.Windows.Forms.TextBox();
+            this.txttatca = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(261, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "label1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(264, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(267, 128);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "label5";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // txtsodau
+            // 
+            this.txtsodau.Location = new System.Drawing.Point(108, 78);
+            this.txtsodau.Name = "txtsodau";
+            this.txtsodau.Size = new System.Drawing.Size(100, 20);
+            this.txtsodau.TabIndex = 3;
+            // 
+            // txtsocuoi
+            // 
+            this.txtsocuoi.Location = new System.Drawing.Point(108, 110);
+            this.txtsocuoi.Name = "txtsocuoi";
+            this.txtsocuoi.Size = new System.Drawing.Size(100, 20);
+            this.txtsocuoi.TabIndex = 4;
+            // 
+            // txttongchan
+            // 
+            this.txttongchan.Location = new System.Drawing.Point(320, 36);
+            this.txttongchan.Name = "txttongchan";
+            this.txttongchan.Size = new System.Drawing.Size(100, 20);
+            this.txttongchan.TabIndex = 5;
+            // 
+            // txttongle
+            // 
+            this.txttongle.Location = new System.Drawing.Point(320, 77);
+            this.txttongle.Name = "txttongle";
+            this.txttongle.Size = new System.Drawing.Size(100, 20);
+            this.txttongle.TabIndex = 6;
+            // 
+            // txttatca
+            // 
+            this.txttatca.Location = new System.Drawing.Point(320, 128);
+            this.txttatca.Name = "txttatca";
+            this.txttatca.Size = new System.Drawing.Size(100, 20);
+            this.txttatca.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(132, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 60);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 349);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txttatca);
+            this.Controls.Add(this.txttongle);
+            this.Controls.Add(this.txttongchan);
+            this.Controls.Add(this.txtsocuoi);
+            this.Controls.Add(this.txtsodau);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtsodau;
+        private System.Windows.Forms.TextBox txtsocuoi;
+        private System.Windows.Forms.TextBox txttongchan;
+        private System.Windows.Forms.TextBox txttongle;
+        private System.Windows.Forms.TextBox txttatca;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ bai14_tinhtoantrongdayso/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai14_tinhtoantrongdayso
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sodau = Convert.ToInt32(txtsodau.Text);
+            int socuoi = Convert.ToInt32(txtsocuoi.Text);
+            int chan = 0;
+            int le = 0;
+            for (int i = sodau; i <= socuoi; i++) 
+            
+                if (i % 2 == i)
+                {
+                    chan += i;
+                }
+                else
+                {
+                    le += i;
+                }
+                txttongchan.Text = chan.ToString();
+                txttongle.Text = le.ToString();
+                txttatca.Text = (chan + le).ToString();
+            }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
